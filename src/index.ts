2409220868aef94ca7ea8130878fb6fd8c2621f0
dobyming <12184874@inha.edu>
2022-12-19 16:05:43 +0900

    defines Type and interface

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,17 @@ interface Contact {
   phones: PhoneNumberDictionary;
 }
 
+enum PhoneTypePlace {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio',
+}
+
 // api
-// TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts() {
-  // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+// Promise의 반환값 정의
+function fetchContacts(): Promise<Contact[]> {
+  // contacts 변수의 반환 type(interface) 선언
+  const contacts: Contact[] = [
     {
       name: 'Tony',
       address: 'Malibu',
@@ -57,42 +63,46 @@ function fetchContacts() {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
   constructor() {
     this.fetchData();
   }
 
-  fetchData() {
+  fetchData(): void {
     fetchContacts().then(response => {
       this.contacts = response;
     });
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-  findContactByName(name) {
+  findContactByName(name: string): Contact[] {
     return this.contacts.filter(contact => contact.name === name);
   }
 
-  findContactByAddress(address) {
+  findContactByAddress(address: string): Contact[] {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+  // phoneType : 1. home, 2. office, 3. studio (enum으로 정리)
+  findContactByPhone(
+    phoneNumber: number,
+    phoneType: PhoneTypePlace
+  ): Contact[] {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
   }
 
-  addContact(contact) {
+  addContact(contact: Contact): void {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
+  displayListByName(): string[] {
     return this.contacts.map(contact => contact.name);
   }
 
-  displayListByAddress() {
+  displayListByAddress(): string[] {
     return this.contacts.map(contact => contact.address);
   }
 }
